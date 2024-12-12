4ad7bc676f4c2559d3971bf606dce40163b09f23
Davidmauri12 <david.tarazona992@gmail.com>
2024-12-11 23:59:09 -0500

    se agrega el proyecto

--- assets/js/helpers/send_form.js
+++ assets/js/helpers/send_form.js
@@ -1,13 +1,13 @@
-const formDOM = document.querySelector('#form');
+const formDOM = document.querySelector('#contact-form');
 const modalDOM = document.querySelector('#modal');
 const closeButttonDOM = document.querySelector('#btn_close-modal');
 
 function openModal() {
-	modalDOM.open = true;
+	modalDOM.style.display = 'block';
 }
 
 function closeModal() {
-	modalDOM.open = false;
+	modalDOM.style.display = 'none';
 }
 
 closeButttonDOM.addEventListener('click', closeModal);
@@ -16,23 +16,26 @@ function sendEmail() {
 	formDOM.addEventListener('submit', (e) => {
 		e.preventDefault();
 		const params = {
-			name: document.getElementById('name').value,
-			email: document.getElementById('email').value,
-			subject: document.getElementById('subject').value,
-			message: document.getElementById('message').value,
+			name: document.getElementById('name').value.trim(),
+			email: document.getElementById('email').value.trim(),
+			subject: document.getElementById('subject').value.trim(),
+			message: document.getElementById('message').value.trim(),
 		};
 
-		if (params.name && params.email && params.message && params.subject) {
-			emailjs.send('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', params).then(
-				(response) => {
-					console.log('SUCCESS!', response.status, response.text);
-					openModal();
-				},
-				(error) => {
-					console.log('FAILED...', error);
-				},
-			);
+		if (!params.name || !params.email || !params.message || !params.subject) {
+			alert('por favor, completa todos los campos antes de enviar el mensaje.');
+			return;
 		}
+		emailjs.send('gmail', 'service_iyhxbbe', params).then(
+			(response) => {
+				console.log('SUCCESS!', response.status, response.text);
+				openModal();
+			},
+			(error) => {
+				console.error('FAILED...', error);
+				alert('hubo un problema al enviar el mensaje. intenta nuevamente');
+			},
+		);
 	});
 }
 
